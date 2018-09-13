72c6e28b34e2b1da0bae407606e2279677cb29ff
Nikaoto <nikaoto@gmail.com>
2018-09-13 23:09:05 +0400

    upd

--- 01/01/main.py
+++ 01/01/main.py
@@ -1,29 +1,35 @@
-# Find all multiples of c between a and b
+'''
+    ნიკა ოთიაშვილი, დავალება 1.1.
+    პოულობს c-ს ჯერადებს a-სა და b-ს შორის.
+'''
+
+# პირველი გზა (ცუდი გზა)
+def first(a, b, c):
+    '''
+    დაუვლის ყველა მთელ რიცხვს a-დან b-მდე, შეამოწმებს არის თუ არა c-ს ჯერადი და
+    დაითვლის მათ. დააბრუნებს რაოდენობას.
+    '''
+    count = 0 # ჯერადების რაოდენობა
+    i = a
+    while i <= b:
+        if i % c == 0:
+            count += 1
+        i += 1
+    return count
+
+# მეორე გზა (უკეთესი, უფრო სწრაფი, გზა)
+def second(a, b, c):
+    '''
+    გაიგებს b-მდე c-ს ჯერადების რაოდენობას, შემდომ (a - 1)-მდე c-ს ჯერადების 
+    რაოდენობას და შემდგომ მათ სხვაობას. a-1 საჭიროა, ვინაიდან b-მდე ჯერადებში 
+    a-ც შეიძლება შევიდეს.
+    '''
+    count = (b // c) - ((a - 1) // c)
+    return count
+
+print("rules: 0 < c < a < b")
 a = int(input("a: "))
 b = int(input("b: "))
 c = int(input("c: "))
 
-def first():
-    op = 1 # operation count
-    count = 0 # multiple count
-    i = a
-    while i <= b:
-        op += 1
-        if i % c == 0:
-            count += 1
-            #print(i)
-        i += 1
-
-    print("count: {}".format(count))
-    print("op: {}".format(op))
-
-def second():
-    op = 2
-    count = (b // c) - ((a - 1) // c)
-    print("count: {}".format(count))
-    print("op: {}".format(op))
-
-print("FIRST:")
-first()
-print("SECOND:")
-second()
+print("count: {}".format(second(a, b, c)))
