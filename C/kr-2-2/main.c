5947b2419e89664ae580aa12c8fc8af68b07de3a
Nikaoto <nikaoto@gmail.com>
2019-02-25 14:30:00 +0400

    add kr-2-2

---
+++ C/kr-2-2/main.c
@@ -0,0 +1,23 @@
+// Author: Nika Otiashvili
+// K&R exercise 2-2
+
+#include <stdio.h>
+
+#define lim 100
+int main(void)
+{
+	 char s[lim];
+	 char c;
+
+	 printf("Input characters until newline:\n");
+	 for (int i=0; i < lim-1; ++i) {
+		  if ((c = getchar()) == '\n')
+			  break;
+		  if (c == EOF)
+			   break;
+		  s[i] = c;
+	 }
+
+	 printf("Chars were %s\n", s);
+	 return 0;
+}
