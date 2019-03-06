d5d63a98279ed576577652bce51d8557bb4f1921
Nikaoto <nikaoto@gmail.com>
2019-03-07 02:18:48 +0400

    clean up kr-1-8

--- C/kr-1-8/main.c
+++ C/kr-1-8/main.c
@@ -1,21 +1,25 @@
-#include "stdio.h"
 // Author: Nikoloz Otiashvili
 // Counts blanks, lines and tabs in input
-int main(int argc, char* argv[]) {
+
+#include "stdio.h"
+
+int main(int argc, char* argv[])
+{
   
-  int c, blank_count, tab_count, line_count;
+	 int c, blank_count, tab_count, line_count;
   
-  while ((c = getchar()) != EOF) {
-    if (c == ' ') {
-      ++blank_count;
-    } else if (c == '\t') {
-      ++tab_count;
-    } else if (c == '\n') {
-      ++line_count;
-    }
-  }
-  printf("\nBlank count: %i\n", blank_count);
-  printf("Tab count: %i\n", tab_count);
-  printf("Line count: %i\n", line_count);
-  return 0;
+	 while ((c = getchar()) != EOF) {
+		  if (c == ' ')
+			   ++blank_count;
+		  else if (c == '\t')
+			   ++tab_count;
+		  else if (c == '\n')
+			   ++line_count;		  
+	 }
+	 
+	 printf("\nBlank count: %i\n", blank_count);
+	 printf("Tab count: %i\n", tab_count);
+	 printf("Line count: %i\n", line_count);
+	 
+	 return 0;
 }
