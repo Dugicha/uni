b1f97cf68c5cbcbeb1009151104bdc654bea6d68
Nikaoto <nikaoto@gmail.com>
2019-03-07 02:19:32 +0400

    clean up kr-1-9

--- C/kr-1-9/main.c
+++ C/kr-1-9/main.c
@@ -1,19 +1,22 @@
-#include "stdio.h"
 // Author: Nikoloz Otiashvili
 // Squashes multiple blanks into one
-int main(void) {
-  int c;
-  int previous_char_blank = 0;
-  
-  while ((c = getchar()) != EOF) {
-    if (c != ' ') {
-      putchar(c);
-      previous_char_blank = 0;
-    } else if (previous_char_blank == 0) {
-      putchar(c);
-      previous_char_blank = 1;
-    }
-  }
-  
-  return 0;
+
+#include "stdio.h"
+
+int main(void)
+{
+	 int c;
+	 int previous_char_blank = 0;
+
+	 while ((c = getchar()) != EOF) {
+		  if (c != ' ') {
+			   putchar(c);
+			   previous_char_blank = 0;
+		  } else if (previous_char_blank == 0) {
+			   putchar(c);
+			   previous_char_blank = 1;
+		  }
+	 }
+
+	 return 0;
 }
