12f86faeadbdc14fad5728d53d43c7cbe15e73f6
Nikaoto <nikaoto@gmail.com>
2019-03-07 02:48:51 +0400

    clean up kr-temperature

--- C/kr-temperature/temperature.c
+++ C/kr-temperature/temperature.c
@@ -1,35 +1,33 @@
+// Author: Nika Otiashvili
+// K&R exercise 1-3 and 1-4
+// Shows a table of temperatures from Celsisus to Fahrenheit
+
 #include <stdio.h>
 
-/* 
-   Shows a table of temperatures from Celsisus to Fahrenheit
-   k&r ex.1-3, 1-4
-*/
+int main()
+{
+	 float fahr, cels;
 
-int main() {
-  float fahr, cels;
-  int lower, upper, step;
-  char* delimiter;
+	 int lower = 0;
+	 int upper = 100;
+	 int step = 10;
+	 char* delimiter = "\t";
 
-  lower = 0;
-  upper = 100;
-  step = 10;
-  delimiter = "\t";
-  
-  // Get widths of the words for formatting table
-  int celsius_width = printf("Celsius");
-  printf("%s", delimiter);
-  int fahrenheit_width = printf("Fahrenheit");
-  printf("\n");
+	 // Get widths of the words for formatting table
+	 int celsius_width = printf("Celsius");
+	 printf("%s", delimiter);
+	 int fahrenheit_width = printf("Fahrenheit");
+	 printf("\n");
 
-  cels = lower;
-  while (cels <= upper) {
-    fahr = cels * (9.0/5.0) + 32;
-    printf("%*.0f", celsius_width, cels);
-    printf("%s", delimiter);
-    printf("%*.1f", fahrenheit_width, fahr);
-    printf("\n");
-    cels += step;
-  }
-  
-  return 0;
+	 cels = lower;
+	 while (cels <= upper) {
+		  fahr = cels * (9.0/5.0) + 32;
+		  printf("%*.0f", celsius_width, cels);
+		  printf("%s", delimiter);
+		  printf("%*.1f", fahrenheit_width, fahr);
+		  printf("\n");
+		  cels += step;
+	 }
+
+	 return 0;
 }
