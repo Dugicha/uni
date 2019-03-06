6d6f631f587760c89cafc5d035efde207e4d223f
Nikaoto <nikaoto@gmail.com>
2019-03-07 02:16:22 +0400

    clean up kr-1-7

--- C/kr-1-7/main.c
+++ C/kr-1-7/main.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
 // Author: Nikoloz Otiashvili
 // Prints value of EOF defined in stdio.h
-int main() {
-  printf("EOF value is %i\n", EOF);
+
+#include <stdio.h>
+
+int main()
+{
+	 printf("EOF value is %i\n", EOF);
 }
