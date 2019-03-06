9f46a1de3c7a39ef41ebe9762a52e65e690c6cc4
Nikaoto <nikaoto@gmail.com>
2019-03-07 02:22:18 +0400

    clean up kr-1-10

--- C/kr-1-10/main.c
+++ C/kr-1-10/main.c
@@ -1,23 +1,26 @@
+// Author: Nikoloz Otiashvili
+// Replaces tabs with "\t", backspaces with "\b", and backslash with "\\"
+
 #include <stdio.h>
-/* 
-   Author: Nikoloz Otiashvili
-   Replaces tabs with "\t", backspaces with "\b", and backslash with "\\"
-*/
-int main(void) {
-  int c;
-  
-  while ((c = getchar()) != EOF) {
-    if (c == '\t') {
-      putchar('\\');
-      putchar('t');
-    } else if (c == '\b') {
-      putchar('\\');
-      putchar('b');
-    } else if (c == '\\') {
-      putchar('\\');
-      putchar('\\');
-    } else {
-      putchar(c);
-    }
-  }
+
+int main(void)
+{
+	 int c;
+
+	 while ((c = getchar()) != EOF) {
+		  if (c == '\t') {
+			   putchar('\\');
+			   putchar('t');
+		  } else if (c == '\b') {
+			   putchar('\\');
+			   putchar('b');
+		  } else if (c == '\\') {
+			   putchar('\\');
+			   putchar('\\');
+		  } else {
+			   putchar(c);
+		  }
+	 }
+
+	 return 0;
 }
