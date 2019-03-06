3bee440ac7518b22183d27d2279d9142795834b0
Nikaoto <nikaoto@gmail.com>
2019-03-07 02:15:51 +0400

    clean up kr-1-6

--- C/kr-1-6/main.c
+++ C/kr-1-6/main.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
 // Author: Nikoloz Otiashvili
 // Verifies whether getchar() != EOF is 0 or 1
-int main() {
-  while((getchar() != EOF) == 0) {
-    printf("Not EOF\n");
-  }
-  printf("EOF\n");
+
+#include <stdio.h>
+
+int main()
+{
+	 while((getchar() != EOF) == 0)
+		  printf("Not EOF\n");
+	 printf("EOF\n");
 }
