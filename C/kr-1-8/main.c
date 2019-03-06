3328c9d98d3e4106001fdd7ca9ba597837fd753f
Nikaoto <nikaoto@gmail.com>
2019-03-07 02:20:20 +0400

    whitespace cleanup

--- C/kr-1-8/main.c
+++ C/kr-1-8/main.c
@@ -5,21 +5,21 @@
 
 int main(int argc, char* argv[])
 {
-  
+
 	 int c, blank_count, tab_count, line_count;
-  
+
 	 while ((c = getchar()) != EOF) {
 		  if (c == ' ')
 			   ++blank_count;
 		  else if (c == '\t')
 			   ++tab_count;
 		  else if (c == '\n')
-			   ++line_count;		  
+			   ++line_count;
 	 }
-	 
+
 	 printf("\nBlank count: %i\n", blank_count);
 	 printf("Tab count: %i\n", tab_count);
 	 printf("Line count: %i\n", line_count);
-	 
+
 	 return 0;
 }
