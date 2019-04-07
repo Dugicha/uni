b076bd66f046c8a59a113343bfa0844bff254b2c
Nikaoto <nikaoto@gmail.com>
2019-04-07 20:11:17 +0400

    add WIP kr-3-1

---
+++ C/kr-3-1/binsearch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n);
+
+int main(void)
+{
+     int v[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+     int x = 6;
+     printf("binsearch(x, v, 10) = %i\n", binsearch(x, v, 10));
+     return 0;
+}
+
+int binsearch(int x, int v[], int n)
+{
+     int low, mid, high;
+
+     low = 0
+     return;
+}
