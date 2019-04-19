46355495f37d3f48c0833de75fd1583295a5d209
Nikaoto <nikaoto@gmail.com>
2019-04-19 19:50:51 +0400

    add kr-4-4

---
+++ C/kr-4-4/charstack.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+size_t bufp = 0;
+
+/* Pop last item in char buffer */
+int getch(void)
+{
+     return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* Push item into char buffer */
+void ungetch(int c)
+{
+     if (bufp >= BUFSIZE)
+	  printf("ungetch: buffer full\n");
+     else
+	  buf[bufp++] = c;
+}
